52725e270ff057cf44c983145a2be84b1cf10da2
Steeves Saillant <steeves.saillant@alithya.com>
2022-02-03 12:10:56 -0500

    added integration test

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -2,8 +2,20 @@ import { mount } from '@cypress/vue'
 import App from './App.vue'
 
 describe('App', () => {
-  it('renders a message', () => {
+  
+  beforeEach(() => {
     mount(App)
+  })
+  it('renders a message', () => {
+    
     cy.get('h1').should('have.text', 'Welcome to Your Vue.js + TypeScript App')
   })
+
+  //integration des composant
+  it('contains and renders the Hello World Component', () => {
+    
+    cy.get('.hello').should("be.visible");
+  })
+
+
 })
